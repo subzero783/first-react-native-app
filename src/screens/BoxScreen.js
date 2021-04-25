1c6d869081f2460ae9883a964418faa2b0559d4a
Gustavo <gusta1296@gmail.com>
2021-04-24 19:44:00 -0700

    left at 73

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,27 @@ import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return(
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>Child #1</Text>
+            <Text style={styles.textStyle}>Child #2</Text>
+            <Text style={styles.textStyle}>Child #3</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderolor: 'black',
+        alignItems: 'center',
+        flexDirection: 'row',
+        height: 200,
+        justifyContent: 'flex-end'
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red'
+    }
+});
 
 export default BoxScreen;
